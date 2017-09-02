3689be2ccd0bce77908ec2baf26d20ea3d67d3df
jas777 <jas.czeszejko@wp.pl>
2017-09-02 16:05:12 +0200

    Add files via upload

---
+++ src/main/java/net/wolfgalaxy/quests/Main.java
@@ -0,0 +1,34 @@
+package net.wolfgalaxy.quests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public void onEnable(){
+
+        System.out.println("Quests enabled.");
+
+        registerCommands();
+        registerEvents();
+
+    }
+
+    public void onDisable(){
+
+
+
+    }
+
+    public void registerCommands(){
+
+
+
+    }
+
+    public void registerEvents(){
+
+
+
+    }
+
+}
